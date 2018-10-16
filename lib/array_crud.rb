0ff8ca452f8c19a63a102a0d0ad30374b91bdb55
agent.karl.zl.ni@gmail.com <agent.karl.zl.ni@gmail.com>
2018-10-16 15:57:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  arr = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  add_element_to_end_of_array (array, element).last
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  arr.unshift (0)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  arr.pop (5)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  arr.delete(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  arr[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  arr.shift
 end
 
 def retrieve_last_element_from_array(array)
+  arr.delete_at (4)
   
 end
